365019e9bde66ffe25dc1ff54df3a2a6d7889918
777moneymaker <adeeeq77@gmail.com>
2022-12-01 11:25:35 +0100

    pre-alpha parser v3

--- phrog_parser.py
+++ phrog_parser.py
@@ -4,20 +4,32 @@ import gffpandas.gffpandas as gffpd
 from io import StringIO
 from pathlib import Path
 import tempfile
+import pickle
 
 parser = argparse.ArgumentParser(
-                    prog = 'TadPole',
-                    description = 'viruses go brrr lol',
-                    epilog = 'Yo')
+    prog="TadPole", description="viruses go brrr lol", epilog="Yo"
+)
 
-parser.add_argument('phrog_dir', type=str)
-parser.add_argument('gff_dir', type=str)           # positional argument
-parser.add_argument('-d', '--max-dist', dest="max_dist", type=int, default = 1, required = False)      # option that takes a value
-parser.add_argument('-a', '--add-number', dest="add_number", type=bool, required = False, default = True)
-parser.add_argument('-c', '--collapse', dest="collapse", type=bool, required = False, default = False)
+parser.add_argument("phrog_dir", type=str)
+parser.add_argument("gff_dir", type=str)  # positional argument
+parser.add_argument(
+    "-d", "--max-dist", dest="max_dist", type=int, default=1, required=False
+)  # option that takes a value
+parser.add_argument(
+    "-a", "--add-number", dest="add_number", type=bool, required=False, default=True
+)
+parser.add_argument(
+    "-c", "--collapse", dest="collapse", type=bool, required=False, default=False
+)
 
 
-def parse_phrog(phrog_dir: Path, gff_dir: Path, max_dist: int, add_number: bool = True, collapse: bool = False) -> list[list]:
+def parse_phrog(
+    phrog_dir: Path,
+    gff_dir: Path,
+    max_dist: int,
+    add_number: bool = True,
+    collapse: bool = False,
+) -> list[list]:
     """
     :param gff_dir: Dir containing gff files
     :param phrog_dir: Dir containing phrog files
@@ -37,7 +49,7 @@ def parse_phrog(phrog_dir: Path, gff_dir: Path, max_dist: int, add_number: bool 
         raise TypeError("Phrog dir is not dir")
     if not gff_dir.is_dir():
         raise TypeError("Gff dir is not dir")
-    
+
     unknown_prot = "joker"
     gff_files = list(gff_dir.iterdir())
     phrog_files = list(phrog_dir.iterdir())
@@ -45,40 +57,53 @@ def parse_phrog(phrog_dir: Path, gff_dir: Path, max_dist: int, add_number: bool 
     gff_files.sort()
     phrog_files.sort()
 
-
     for gff_file, phrog_file in zip(gff_files, phrog_files):
         with open(gff_file, "r") as fh:
             text = "".join(line.rstrip(";") for line in fh)
         with tempfile.TemporaryDirectory() as td:
-            f_name = Path(td) / 'test'
-            with open(f_name, 'w') as fh:
+            f_name = Path(td) / "test"
+            with open(f_name, "w") as fh:
                 fh.write(text)
             gff_data = gffpd.read_gff3(f_name).attributes_to_columns()
-        
+
         phrogs = pd.read_csv(phrog_file)
         phrogs = phrogs.rename(columns={"prot_id": "ID"})
-        
-        df_phrogs = pd.merge(phrogs, gff_data, how="right", on="ID")[["ID", "hmm_id", "seq_id", "start", "end", "strand"]]
+
+        df_phrogs = pd.merge(phrogs, gff_data, how="right", on="ID")[
+            ["ID", "hmm_id", "seq_id", "start", "end", "strand"]
+        ]
         df_phrogs.fillna(unknown_prot, inplace=True)
 
         # g = df_phrogs.groupby((df_phrogs['strand'].shift() != df_phrogs["strand"]).cumsum())
         phrogs = df_phrogs["hmm_id"].values.tolist()
         strands = df_phrogs["strand"].values.tolist()
-        
+        starts = df_phrogs["start"].values.tolist()[1:]
+        ends = df_phrogs["end"].values.tolist()[:-1]
+        dists = [float(s) - float(e) for s, e in zip(starts, ends)]
+        # Jest good
+
+        print(dists)
+
         curr_strand = strands[0]
         paragraph = []
         word = []
         for p, s in zip(phrogs, strands):
             pass
-                
-            
+        
+        return paragraph
 
 
 def main():
     args = parser.parse_args()
     phrog_dir = Path(args.phrog_dir)
     gff_dir = Path(args.gff_dir)
-    parse_phrog(phrog_dir=phrog_dir, gff_dir=gff_dir, max_dist=args.max_dist, add_number=args.add_number, collapse=args.collapse)
+    parse_phrog(
+        phrog_dir=phrog_dir,
+        gff_dir=gff_dir,
+        max_dist=args.max_dist,
+        add_number=args.add_number,
+        collapse=args.collapse,
+    )
 
 
 if __name__ == "__main__":
