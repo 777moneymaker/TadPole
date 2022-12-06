2773d605af930480e7c06179a9dccd7fad79545a
phenolophthaleinum <mccv99@gmail.com>
2022-12-06 11:34:19 +0100

    refactored word2vec_train: logging, using utils, timer

--- word2vec_train.py
+++ word2vec_train.py
@@ -7,31 +7,39 @@ import dill
 import pickle
 import utils
 import plotly
+from pathlib import Path
+import custom_logger
+import time
+import re
 
+
+#TODO: make this whole file like a proper module
+
+# start time
+tic = time.perf_counter()
 
 #  *** Word2vec ***
 # corpus
-# there will be binary object read with corpus as list of lists
-
 #read pickle
-# TODO: pickle change to dill and maybe add to utils.py    
-with open('results/vir2000_collapsed.pickle', 'rb') as f:
-    sentences = pickle.load(f)
-print("Load pickle")
+# sentences = utils.read_corpus(Path("results/vir2000_collapsed.pickle"))
+sentences = utils.read_corpus(Path("results/vir2000_numbered.pickle"))
+# are jokers numbered?; btw this is not ideal, but it will all be changing
+numbered = True
 
 # train model (simplest form possible)
-model = wv.Word2Vec(sentences, min_count=1)
+custom_logger.logger.info("Creating Word2vec model")
+model = wv.Word2Vec(sentences, min_count=1, workers=4)
+
 # save model to binary file
-model.save("train_test/test.model")
-print("Save model")
-#print(model.wv.index_to_key)
+# model.save("train_test/test.model")
+# print("Save model")
+
 # get embedded vectors from the model
 vectors = model.wv
-#print(vectors.__dict__)
 
 # save embedded vectors to binary file
-vectors.save("train_test/test.wordvectors")
-print("Save vectors")
+# vectors.save("train_test/test.wordvectors")
+# print("Save vectors")
 
 #  *** junk - skip ***
 # d = np.vectorize(vectors.index_to_key.get)(vectors.vectors)
@@ -42,35 +50,46 @@ print("Save vectors")
 
 #  *** UMAP ***
 # initialise UMAP
+custom_logger.logger.info("UMAP Magic")
 reducer = umap.UMAP(n_components=3)
 # data_to_reduce = dataset['vector'].to_list()
 data_to_reduce = vectors.vectors
 # reduce dimensionality
 embedding = reducer.fit_transform(data_to_reduce)
 # list of embeddings with reduced dims to n=3
-print("UMAP Magick")
-print(embedding)
+# print(embedding)
 
 #  *** Visualisation ***
+# get functions from metadata
+custom_logger.logger.info("Loading phrog metadata")
+func = utils.read_metadata(Path("Data/metadata_phrog.dill"))
 # gather data to dataframe
+custom_logger.logger.info("Gathering data for visualisation")
 dataset = pd.DataFrame({'word': vectors.index_to_key})
-# map functions to words
-#TODO: refactor to function
-#read dill  with functions
-with open('Data/metadata_phrog.dill', 'rb') as in_strm:
-    func = dill.load(in_strm)
-
 #add joker to func
-func['joker']  = 'joker_placeholder'
-
+if not numbered:
+    func['joker']  = 'joker_placeholder'
+# silly joker name enhancing part
+if numbered:
+    pattern = re.compile(r"joker\d+")
+    jokers = [x for l in [list(filter(pattern.match, elem)) for elem in sentences] for x in l]
+    print(jokers)
+    joker_funcs = {joker: "joker_placeholder" for joker in jokers}
+    func.update(joker_funcs)
+# print(func)
+# map functions to words
 dataset["function"] = dataset['word'].map(func)
-
 # insert embedding data
 dataset[['x', 'y', 'z']] = pd.DataFrame(embedding, index=dataset.index)
-print(dataset)
+# print(dataset)
 
 # show plot
+custom_logger.logger.info("Creating visualisation")
 fig = px.scatter_3d(dataset, x='x', y='y', z='z', color='function', hover_data=["word"])
 plotly.offline.plot(fig, filename='plots/vir2000.html')
 fig.show()
+
+toc = time.perf_counter()
+elapsed_time = toc - tic
+custom_logger.logger.info(f"Done in {elapsed_time:0.8f}")
 
