29e9fd875230d71d80c91ec387450921b6d59a0a
Ada Gazecka <gazecka991@gmail.com>
2022-12-12 18:33:09 +0100

    Added three scoring functions: max, sum, mean

--- evaluation.py
+++ evaluation.py
@@ -2,9 +2,26 @@ from pathlib import Path
 import pandas as pd
 from typing import Union
 from gensim.models import FastText, Word2Vec
+from collections import defaultdict
 
 import utils
 
+def sum_tuples(lst: tuple[str, float]):
+    d = defaultdict(float)
+    for category, prob in lst:
+        d[category] += prob
+    
+    return list(d.items())
+    
+def mean_tuples(lst: tuple[str, float]):
+    d = defaultdict(float)
+    occurs = defaultdict(int)
+    for category, prob in lst:
+        d[category] += prob
+        occurs[category] += 1
+    for category in d:
+        d[category] /= occurs[category]
+    return list(d.items())
 
 def prediction(
     func_dict: dict,
@@ -16,25 +33,55 @@ def prediction(
 
     # create a list of phrogs with known function
     known_func_phrog_list = df[(df['category'] != 'unknown function')]['phrog_id'].tolist()
+    # TODO: Consider making this a set, O(1) membership checking
+    # known_func_phrog_list = set(known_func_phrog_list)
 
     # select matches with known function
     #for phrog in known_func_phrog_list:   # <-- TODO: consider changing it to multiprocessing
+
+    # Dictionary phrog: {
+    #   scoring_function: category
+    # }
+    # Example:
+    # 'phrog13': {
+    #   'max': ('category1', 0.923),
+    #   'sum': ('category3', 0.987),
+    #   'mean': ('category1', 0.956)
+    # }
+    phrog_categories: dict[str, dict[str, str]] = {}
+
     for phrog in known_func_phrog_list[:10]: # placeholder, model wasn't trained for all phrogs yet, tested to work on phrogs1-10
-        vectors = model.wv
-        result = vectors.most_similar(phrog, topn = 40000) # <-- TODO: looking for ideas how to optimize this
+        vectors = model.wv  
+        result = vectors.most_similar(phrog, topn = 40_000) # Use _ for clarity # <-- TODO: looking for ideas how to optimize this
         #result = (list(filter(lambda x: 'joker' not in x[0], result)))  # to remove jokers from result; turns out mergeddf_to_tuple isnt returning them anyway so far
-        #print(result)
 
         # replace phrogs with functions
         tuples_to_df = pd.DataFrame(result, columns=['phrog_id', 'probability'])
-        merged = tuples_to_df.merge(df, left_on='phrog_id', right_on='phrog_id')
-        merged.drop(merged.index[merged['category'] == 'unknown function'], inplace = True) # remove phrogs with unknown function
-        merged = merged.head(50) # select only top 50
+        merged = tuples_to_df.merge(df, on='phrog_id') # Same col, just use on=
+        
+        # just use loc, add .head immeadiately
+        merged = merged.loc[merged['category'] != 'unknown function'].head(50)
         merged_id_category = merged[["category", "probability"]]
-        mergeddf_to_tuple = list(merged_id_category.itertuples(index=False, name=None))
-        #print(mergeddf_to_tuple)
 
-        # TODO: scoring functions
+        # Begin scoring
+        tpls = list(merged_id_category.apply(tuple, 1))
+        key_func = lambda x: x[1]
+        
+        # 3 scoring functions
+        # mx: max value for a category
+        # sum: max value after summing prob for each category
+        # mean: max value after taking a mean prob for each category
+        mx = max(tpls, key=key_func)
+        summed = max(sum_tuples(tpls), key=key_func)
+        mean = max(mean_tuples(tpls), key=key_func)
+
+        
+        phrog_categories[phrog] = {
+            "max": mx,
+            "sum": summed,
+            "mean": mean
+        }
+        # End scoring
 
     # TODO: validation
 
