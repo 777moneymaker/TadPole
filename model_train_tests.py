2824acec01ae6b63bd5df04bf64f91d2d24cf86d
phenolophthaleinum <mccv99@gmail.com>
2023-04-08 18:20:48 +0200

    dirty try to optimize w2v

--- model_train_tests.py
+++ model_train_tests.py
@@ -171,51 +171,55 @@ import bayes_optimization as bay
 # pipe.run()
 
 # opt
-# pipe = w2v.Word2VecPipeline(
-#     corpus_path="results/virall_encode_02-04-2023.pickle",
-#     output_prefix="virall_opt",
-#     metadata="Data/metadata_02-04-2023.pickle",
-#     vector_size=20,
-#     window=2,
-#     min_count=5,
-#     epochs=5,
-#     workers=40,
-#     lr_start=0.005,
-#     lr_min=0.0001,
-#     hs=0,
-#     negative=75,
-#     ns_exp=-0.1,
-#     callbacks=[w2v.TrainLogger()],
-#     visualise_model=False,
-#     encoded=True,
-#     save_model= False
-# )
-
-pipe = ft.FastTextPipeline(
+pipe = w2v.Word2VecPipeline(
     corpus_path="results/virall_encode_02-04-2023.pickle",
-    output_prefix="ft_virall_opt_errorDebug",
+    output_prefix="virall_optProto_w2v",
     metadata="Data/metadata_02-04-2023.pickle",
     vector_size=20,
     window=2,
     min_count=5,
     epochs=5,
     workers=40,
-    # lr_start=0.005,
-    # lr_min=0.0001,
-    lr_start=1,
+    lr_start=0.005,
     lr_min=0.0001,
     hs=0,
     negative=75,
-    ns_exp=-0.75,
+    ns_exp=-0.1,
+    callbacks=[w2v.TrainLogger()],
     visualise_model=False,
     encoded=True,
+    save_model= False
 )
 
+# pipe = ft.FastTextPipeline(
+#     corpus_path="results/virall_encode_02-04-2023.pickle",
+#     output_prefix="ft_virall_opt_errorDebugFinal2",
+#     metadata="Data/metadata_02-04-2023.pickle",
+#     vector_size=20,
+#     window=2,
+#     min_count=5,
+#     epochs=5,
+#     workers=40,
+#     # lr_start=0.005,
+#     # lr_min=0.0001,
+#     lr_start=1,
+#     lr_min=0.0001,
+#     hs=0,
+#     negative=75,
+#     ns_exp=-0.75,
+#     visualise_model=False,
+#     encoded=True,
+#     save_model=False
+# )
+
 hypers = {
-    'vector_size': (20, 50),
+    'vector_size': (50, 200),
+    'epochs': (100, 500),
     'ns_exp': (-0.75, 0.75),
-    'lr_start': (0.5, 1)
+    'lr_start': (0.001, 0.5),
+    'lr_min': (0.0001, 0.1),
+    'negative': (45, 135)
 }
 
-bayes = bay.BayesianOptimizer(pipe, hypers, 2, 3, "ft_test_opt_errorDebug", Path("./logs/ft_test_opt_07_04_errorDebug"))
+bayes = bay.BayesianOptimizer(pipe, hypers, 5, 20, "w2v_optProto", Path("./logs/w2v_optProto"))
 bayes.optimize()
