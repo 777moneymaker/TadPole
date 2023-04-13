2eddbd28596898edb562147a7e12fd69a441a64c
phenolophthaleinum <mccv99@gmail.com>
2023-04-13 22:23:46 +0200

    typo in path

--- new_evaluation_test.py
+++ new_evaluation_test.py
@@ -5,6 +5,6 @@ from pathlib import Path
 
 
 funcs = utils.read_metadata(Path("Data/metadata_02-04-2023.pickle"))
-model = FastText.load("virall_optProto_w2v_ns-049525437065314665_lr004343706147351918_lrmin008947120568403436_d181_w2_e450_hs0_neg48_mincount5.model")
+model = FastText.load("logs/w2v_optProto/virall_optProto_w2v_ns-049525437065314665_lr004343706147351918_lrmin008947120568403436_d181_w2_e450_hs0_neg48_mincount5.model")
 model_name = "virall_optProto_w2v_ns-049525437065314665_lr004343706147351918_lrmin008947120568403436_d181_w2_e450_hs0_neg48_mincount5"
 eval = evaluation.prediction(func_dict=funcs, model=model, model_name=model_name)
