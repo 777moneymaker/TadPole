b1809b3252d99eed309aa146f0b3988e720966e1
phenolophthaleinum <mccv99@gmail.com>
2023-04-13 22:21:02 +0200

    prepare for new eval test

---
+++ new_evaluation_test.py
@@ -0,0 +1,10 @@
+import evaluation
+import utils
+from gensim.models import FastText
+from pathlib import Path
+
+
+funcs = utils.read_metadata(Path("Data/metadata_02-04-2023.pickle"))
+model = FastText.load("virall_optProto_w2v_ns-049525437065314665_lr004343706147351918_lrmin008947120568403436_d181_w2_e450_hs0_neg48_mincount5.model")
+model_name = "virall_optProto_w2v_ns-049525437065314665_lr004343706147351918_lrmin008947120568403436_d181_w2_e450_hs0_neg48_mincount5"
+eval = evaluation.prediction(func_dict=funcs, model=model, model_name=model_name)
