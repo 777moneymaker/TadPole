68a49b8128d9541c48eaa69869ffda9e23ce3092
phenolophthaleinum <mccv99@gmail.com>
2023-05-05 00:24:38 +0200

    added missing positional for opt domain reductuion

--- model_train_tests.py
+++ model_train_tests.py
@@ -477,7 +477,7 @@ hypers = {
 # }
 
 
-bayes = bay.BayesianOptimizer(pipe, hypers, 8, 27, "aus_w2v_2nd_followup_domainreduction", Path("./logs/aus_w2v_2nd_followup_domainreduction"), aquisition_function='ucb', kappa=7.2)
+bayes = bay.BayesianOptimizer(pipe, hypers, 8, 27, "aus_w2v_2nd_followup_domainreduction", Path("./logs/aus_w2v_2nd_followup_domainreduction"), aquisition_function='ucb', kappa=7.2, domain_reduction=True)
 # bayes = bay.BayesianOptimizer(pipe, quick_hypers, 2, 2, "callback_test", Path("./logs/callback_test"), aquisition_function='ucb', kappa=10, domain_reduction=True)
 bayes.optimize()
 
